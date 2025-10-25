51e78b47dfb562bf69163f3a7ea5f865a971672b
aarish21 <aarish.jerry@gmail.com>
2025-10-26 02:04:21 +0530

    added location

--- src/main/java/com/aarish/safai_setu/controller/ReportController.java
+++ src/main/java/com/aarish/safai_setu/controller/ReportController.java
@@ -1,11 +1,10 @@
 package com.aarish.safai_setu.controller;
 
-import org.springframework.http.MediaType;
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
